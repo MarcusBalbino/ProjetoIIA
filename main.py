7981d144160534d2cc8ffa8af6830dea96f50b2c
unknown <marcus.vbcavalcante@gmail.com>
2025-05-22 21:23:26 -0300

    Passando o jupyter notebook e atualizando o código na main e formatando o arquito localizacao.txt

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello, world")
+# Transformando o OlericolasFrutas.txt em um dataframe e convertendo para um csv
+
+with open("OlericolasFrutas.txt", "r", encoding="utf-8") as file:
+    for linha in file:
+        print(f'{linha.strip()}')
