19603374c18480a4a43c9d3040ee56db5e938d14
unknown <marcus.vbcavalcante@gmail.com>
2025-05-24 14:30:32 -0300

    Alterando nome de arquivo e adicionando funcionalidade na main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # Transformando o OlericolasFrutas.txt em um dataframe e convertendo para um csv
+dic_localizacao = {}
 
-with open("OlericolasFrutas.txt", "r", encoding="utf-8") as file:
-    for linha in file:
-        print(f'{linha.strip()}')
+with open("localizacao.txt", "r", encoding="utf-8") as file:
+    for linha in file.readline():
+        nome, coordenadas = linha.split(',')
+        dic_localizacao[nome] = coordenadas
+print(dic_localizacao)
