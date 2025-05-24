129e6436add557f1ad04e9178b569e07af0000cd
unknown <marcus.vbcavalcante@gmail.com>
2025-05-24 14:45:41 -0300

    Alterando uma funcionalidade na main.py

--- main.py
+++ main.py
@@ -1,8 +1,58 @@
 # Transformando o OlericolasFrutas.txt em um dataframe e convertendo para um csv
-dic_localizacao = {}
+associacoes_cooperativas = {
+    "Afeca": {"nome": "Associação de Agricultores Familiares da Eco Comunidade do Assentamento 15 de Agosto", "coordenadas": (-15.911257322468854, -47.72131987698071)},
+    "Agrifam": {"nome": "Associação Agrícola do Distrito Federal e Ride", "coordenadas": (-16.033332269528525, -48.02194559697238)},
+    "Amista": {"nome": "Associação Mista dos Agricultores Familiares, Orgânicos e Produtores Rurais do DF e Entorno", "coordenadas": (-15.763959936767963, -47.49298390178075)},
+    "Aspaf": {"nome": "Associação dos Produtores da Agricultura Familiar", "coordenadas": (-15.680704256671605, -48.204481263228885)},
+    "AsSpag": {"nome": "Associação dos Produtores Rurais de Alexandre Gusmão", "coordenadas": (-15.756719533443205, -48.12660143651946)},
+    "Asphor": {"nome": "Associação dos Produtores de Hortigranjeiros do Distrito Federal", "coordenadas": (-15.796015013316007, -47.95031196939317)},
+    "Asproc": {"nome": "Associação dos Produtores Rurais Orgânicos e Convencionais do Distrito Federal e Entorno", "coordenadas": (-15.671024486215536, -48.13831575695135)},
+    "Aspronte": {"nome": "Associação dos Produtores Rurais Novo Horizonte Betinho", "coordenadas": (-15.598206767692025, -48.124069244099275)},
+    "Astraf": {"nome": "Associação dos Trabalhadores Rurais da Agricultura Familiar do Assentamento Chapadinha", "coordenadas": (-15.542807850627778, -48.02983303362831)},
+    "Coopbrasil": {"nome": "Cooperativa Mista da Agricultura Familiar do Meio Ambiente e da Cultura do Brasil", "coordenadas": (-16.25490982477886, -47.95523828036136)},
+    "Cooper-Horti": {"nome": "Cooperativa Agrícola Buriti Vermelho", "coordenadas": (-15.898669835597419, -47.40685178022046)},
+    "Prorural": {"nome": "Cooperativa dos Produtores Rurais de Planaltina de Goiás e Região", "coordenadas": (-15.44566180715857, -47.61898200913563)},
+    "Coopebraz": {"nome": "Cooperativa Agropecuária da Região de Brazlândia", "coordenadas": (-15.598018611676984, -48.11806404508394)},
+    "Coopermista": {"nome": "Cooperativa de Agricultura Familiar Mista do Distrito Federal", "coordenadas": (-15.764173354290381, -47.49167060443745)},
+    "Rede Terra": {"nome": "Cooperativa dos Agricultores Familiares Ecológicos do Cerrado", "coordenadas": (-16.749588317495615, -47.61598414282499)},
+    "Cootaquara": {"nome": "Cooperativa Agrícola da Região de Planaltina", "coordenadas": (-15.633270200233193, -47.52254805570911)},
+    "Cooperbrasília": {"nome": "Cooperativa de Serviços Ambientais, Agricultura Familiar, Sociedade, Cultura e Saúde", "coordenadas": (-15.938126193908477, -47.82535903093143)}
+}
 
-with open("localizacao.txt", "r", encoding="utf-8") as file:
-    for linha in file.readline():
-        nome, coordenadas = linha.split(',')
-        dic_localizacao[nome] = coordenadas
-print(dic_localizacao)
+alimentos = {
+    "Alface": {"tipo": "hortaliça", "sazonalidade": "ano todo", "nutrientes": ["fibras", "vitamina A", "vitamina K"]},
+    "Mandioca": {"tipo": "raiz", "sazonalidade": "ano todo", "nutrientes": ["carboidratos", "fibras", "vitamina B6"]},
+    "Tomate": {"tipo": "fruta", "sazonalidade": "verão", "nutrientes": ["licopeno", "vitamina C", "potássio"]},
+    "Repolho": {"tipo": "hortaliça", "sazonalidade": "outono e inverno", "nutrientes": ["vitamina C", "fibras", "antioxidantes"]},
+    "Batata": {"tipo": "tubérculo", "sazonalidade": "ano todo", "nutrientes": ["carboidratos", "potássio", "vitamina B6"]},
+    "Cebola": {"tipo": "hortaliça", "sazonalidade": "ano todo", "nutrientes": ["antioxidantes", "vitamina C", "fibras"]},
+    "Couve": {"tipo": "hortaliça", "sazonalidade": "inverno", "nutrientes": ["ferro", "vitamina K", "fibras"]},
+    "Chuchu": {"tipo": "hortaliça", "sazonalidade": "ano todo", "nutrientes": ["fibras", "potássio", "vitamina C"]},
+    "Morango": {"tipo": "fruta", "sazonalidade": "primavera e verão", "nutrientes": ["vitamina C", "antioxidantes", "fibras"]},
+    "Pimentão": {"tipo": "hortaliça", "sazonalidade": "verão", "nutrientes": ["vitamina C", "carotenóides", "antioxidantes"]},
+    "Brócolis": {"tipo": "hortaliça", "sazonalidade": "inverno", "nutrientes": ["vitamina K", "vitamina C", "fibras"]},
+    "Abóbora": {"tipo": "hortaliça", "sazonalidade": "outono", "nutrientes": ["betacaroteno", "vitamina A", "fibras"]},
+    "Berinjela": {"tipo": "hortaliça", "sazonalidade": "verão", "nutrientes": ["fibras", "antioxidantes", "vitamina B6"]},
+    "Beterraba": {"tipo": "raiz", "sazonalidade": "ano todo", "nutrientes": ["ferro", "ácido fólico", "antioxidantes"]},
+    "Pepino": {"tipo": "hortaliça", "sazonalidade": "verão", "nutrientes": ["fibras", "vitamina K", "potássio"]},
+    "Cenoura": {"tipo": "raiz", "sazonalidade": "ano todo", "nutrientes": ["betacaroteno", "vitamina A", "fibras"]},
+    "Quiabo": {"tipo": "hortaliça", "sazonalidade": "verão", "nutrientes": ["fibras", "vitamina C", "antioxidantes"]},
+    "Agrião": {"tipo": "hortaliça", "sazonalidade": "inverno", "nutrientes": ["vitamina K", "ferro", "antioxidantes"]},
+    "Jiló": {"tipo": "hortaliça", "sazonalidade": "verão", "nutrientes": ["antioxidantes", "vitamina B6", "fibras"]},
+    "Gengibre": {"tipo": "raiz", "sazonalidade": "ano todo", "nutrientes": ["antioxidantes", "anti-inflamatórios", "vitamina C"]},
+    "Abacate": {"tipo": "fruta", "sazonalidade": "verão", "nutrientes": ["gorduras saudáveis", "potássio", "vitamina E"]},
+    "Goiaba": {"tipo": "fruta", "sazonalidade": "verão", "nutrientes": ["vitamina C", "licopeno", "fibras"]},
+    "Banana": {"tipo": "fruta", "sazonalidade": "ano todo", "nutrientes": ["potássio", "vitamina B6", "fibras"]},
+    "Limão": {"tipo": "fruta", "sazonalidade": "ano todo", "nutrientes": ["vitamina C", "antioxidantes", "fibras"]},
+    "Tangerina": {"tipo": "fruta", "sazonalidade": "inverno", "nutrientes": ["vitamina C", "fibras", "antioxidantes"]},
+    "Maracujá": {"tipo": "fruta", "sazonalidade": "verão", "nutrientes": ["fibras", "vitamina C", "antioxidantes"]},
+    "Manga": {"tipo": "fruta", "sazonalidade": "verão", "nutrientes": ["vitamina A", "fibras", "antioxidantes"]},
+    "Lichia": {"tipo": "fruta", "sazonalidade": "verão", "nutrientes": ["vitamina C", "antioxidantes", "fibras"]},
+    "Uva": {"tipo": "fruta", "sazonalidade": "outono", "nutrientes": ["antioxidantes", "vitamina C", "fibras"]},
+    "Atemóia": {"tipo": "fruta", "sazonalidade": "verão", "nutrientes": ["vitamina C", "potássio", "fibras"]},
+    "Cajamanga": {"tipo": "fruta", "sazonalidade": "verão", "nutrientes": ["vitamina C", "antioxidantes", "fibras"]},
+    "Graviola": {"tipo": "fruta", "sazonalidade": "verão", "nutrientes": ["fibras", "vitamina C", "potássio"]},
+    "Coco": {"tipo": "fruta", "sazonalidade": "ano todo", "nutrientes": ["gorduras saudáveis", "potássio", "fibras"]},
+    "Pitaia": {"tipo": "fruta", "sazonalidade": "verão", "nutrientes": ["antioxidantes", "vitamina C", "fibras"]},
+    "Mamão": {"tipo": "fruta", "sazonalidade": "ano todo", "nutrientes": ["vitamina A", "fibras", "antioxidantes"]},
+}
